f7e034a1345f323de5fab97fbdfb5d171e067885
Fer Perales <fer.perales.montoya@gmail.com>
2013-05-12 09:39:57 -0500

    Change app inner structure

--- index.rb
+++ index.rb
@@ -1,67 +1,90 @@
 require 'rubygems'
 require 'sinatra'
-require 'mechanize'
+require 'nokogiri'
 require 'json'
 require 'sass'
+
+#Declare URL for screen scrapping
+WORKSHOPS_DAY_1_URL = 'http://divecfest.cucei.udg.mx/t1.php?fecha=13'
+WORKSHOPS_DAY_2_URL = 'http://divecfest.cucei.udg.mx/t1.php?fecha=14'
+CONFERENCES_DAY_1_URL = 'http://divecfest.cucei.udg.mx/conferencias.html'
+CONFERENCES_DAY_2_URL = 'http://divecfest.cucei.udg.mx/conferencias1.html'
 
 get '/' do
 	@title = "DivecFest 2013"
   haml :index
 end
 
-get '/workshops' do
-	@title = "DivecFest 2013 : Workshops"
-  haml :workshops, :workshops => get_workshops_schedule
+get '/workshops/day1' do
+	@title = "DivecFest 2013 : Talleres día 1"
+  @workshops = get_workshops_day1
+  haml :workshops
 end
 
-get '/workshops.json' do
-  get_workshops_schedule.to_json
+get '/workshops/day1.json' do
+  get_workshops_day1.to_json
+end
+
+get '/workshops/day2' do
+  @title = "DivecFest 2013 : Talleres día 2"
+  @workshops = get_workshops_day2
+  haml :workshops
+
+end
+
+get '/workshops/day2.json' do
+  get_workshops_day2.to_json
 end
 
 
 get '/conferences/day1' do
+  @title = "DivecFest 2013 : Conferencias día 1"
+  @conferences = get_conferences_day1 
+  haml :conferences
 end
 
 get '/conferences/day1.json' do
+  get_conferences_day1.to_json
 end
 
 get '/conferences/day2' do
+  @title = "DivecFest 2013 : Conferencias día 2"
+  @conferences = get_conferences_day1 
+  haml :conferences
 end
 
 get '/conferences/day2.json' do
+  get_conferences_day2.to_json
 end
 
 get '/*' do
-  return "404 ERROR"
+  haml :error
 end
 
+# GET SCHEDULE BY DAY
 
-# Common methods
+def get_workshops_day1
+  get_workshops_schedule WORKSHOPS_DAY_1_URL
+end
 
-def get_workshops_schedule
+def get_workshops_day2
+  get_workshops_schedule WORKSHOPS_DAY_2_URL
+end
 
-  agent = Mechanize.new
-  workshops_page = conference_page = agent.get('http://divecfest.cucei.udg.mx/talleres.html')
+def get_conferences_day1
+  get_conferences_schedule CONFERENCES_DAY_1_URL
+end
 
-  header = workshops_page.search('.ui-widget-header>tr')
-  headers_array = []
+def get_conferences_day2
+  get_conferences_schedule CONFERENCES_DAY_2_URL
+end
 
-  header.search('td').each do |x|
-    headers_array << x.text
-  end	
+# COMMOM METHODS FOR PARSING
 
-  body = workshops_page.search('.ui-widget.content>tr')
-  workshops_hash = {}
-  hash_index = 0
+def get_conferences_schedule url
+  {}
+end
 
-  body.each do |x|
-    hsh = {}
-    x.search('td').each_with_index do |y, i|
-      hsh[headers_array[i]] = y.text
-    end
-    workshops_hash[hash_index] = hsh
-    hash_index += 1
-  end
-
-  workshops_hash
+def get_workshops_schedule url
+  {}
 end
